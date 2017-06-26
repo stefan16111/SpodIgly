078992802a6b833c6b88858c2acb6bb12ee35275
stefan16111@wp.pl <stefan16111@wp.pl>
2017-06-26 14:27:52 +0200

    List and partial view

--- SpodIgly/Controllers/StoreController.cs
+++ SpodIgly/Controllers/StoreController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpodIgly.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,8 @@ namespace SpodIgly.Controllers
 {
     public class StoreController : Controller
     {
-        // GET: Store
+        StoreContext db = new StoreContext();
+
         public ActionResult Index()
         {
             return View();
@@ -16,14 +18,23 @@ namespace SpodIgly.Controllers
 
         public ActionResult Details(int id)
         {
-
             return View();
         }
 
         public ActionResult List(string genrename)
         {
+            var genre = db.Genres.Include("Albums").Where(g => g.Name.ToUpper() == genrename.ToUpper()).Single();
+            var albums = genre.Albums.ToList();
 
-            return View();
+            return View(albums);
+        }
+
+        [ChildActionOnly]
+        public ActionResult GenresMenu()
+        {
+            var genres = db.Genres.ToList();
+
+            return PartialView("_GenresMenu", genres);
         }
     }
 }
