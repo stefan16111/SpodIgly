9d95f8ec85c358a7aeda7bb69195fd3b65dd75d3
stefan16111@wp.pl <stefan16111@wp.pl>
2017-06-29 20:33:03 +0200

    sitemap navigation

---
+++ SpodIgly/Infrastructures/ProductDetailsDynamicNodeProvider.cs
@@ -0,0 +1,32 @@
+﻿using MvcSiteMapProvider;
+using SpodIgly.DAL;
+using SpodIgly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpodIgly.Infrastructures
+{
+    public class ProductDetailsDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private StoreContext db = new StoreContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var returnValue = new List<DynamicNode>();
+
+            foreach(Album a in db.Albums)
+            {
+                DynamicNode n = new DynamicNode();
+                n.Title = a.AlbumTitle;
+                n.Key = "Album_" + a.AlbumId;
+                n.ParentKey = "Genre_" + a.GenreId;
+                n.RouteValues.Add("id", a.AlbumId);
+                returnValue.Add(n);
+            }
+
+            return returnValue;
+        }
+    }
+}
---
+++ SpodIgly/Infrastructures/ProductListDynamicNodeProvider.cs
@@ -0,0 +1,31 @@
+﻿using MvcSiteMapProvider;
+using SpodIgly.DAL;
+using SpodIgly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpodIgly.Infrastructures
+{
+    public class ProductListDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private StoreContext db = new StoreContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var returnValue = new List<DynamicNode>();
+
+            foreach (Genre g in db.Genres)
+            {
+                DynamicNode n = new DynamicNode();
+                n.Title = g.Name;
+                n.Key = "Genre_" + g.GenreId;
+                n.RouteValues.Add("genrename", g.Name);
+                returnValue.Add(n);
+            }
+
+            return returnValue;
+        }
+    }
+}
