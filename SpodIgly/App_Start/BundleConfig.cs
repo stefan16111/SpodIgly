f67f275461f8bf9d60b393819c926505e7ac7d2e
stefan16111@wp.pl <stefan16111@wp.pl>
2017-06-30 19:24:50 +0200

    add script js and create bundle js

---
+++ SpodIgly/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SpodIgly.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundle(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js") );
+        }
+    }
+}
