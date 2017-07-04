cb67e4c4bd0d7d1e4e19ee1f8b64f152997e3809
stefan16111@wp.pl <stefan16111@wp.pl>
2017-07-04 21:47:39 +0200

    login view

---
+++ SpodIgly/ViewModels/AccountViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SpodIgly.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "musisz wprowadzić email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "musisz wprowadzić hasło")]
+        public string Password { get; set; }
+
+        [Display(Name = "Zapamiętaj mnie")]
+        public bool RememberMe { get; set; }
+
+    }
+}
