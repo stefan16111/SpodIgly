3a9e1b4c4006f5a95aa97c20a2d7a332d01f0ffe
stefan16111@wp.pl <stefan16111@wp.pl>
2017-07-04 22:43:14 +0200

    register action

--- SpodIgly/Controllers/AccountController.cs
+++ SpodIgly/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpodIgly.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,37 @@ namespace SpodIgly.Controllers
         {
             ViewBag.ReturnUrl = returnUrl;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
         }
     }
 }
--- SpodIgly/ViewModels/AccountViewModels.cs
+++ SpodIgly/ViewModels/AccountViewModels.cs
@@ -20,4 +20,23 @@ namespace SpodIgly.ViewModels
         public bool RememberMe { get; set; }
 
     }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "the {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "The Password and confirmation password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
 }
